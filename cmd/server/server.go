0ba2ab325fa57cc71f041546431ff944d93c7b9b
gabrielgrc <gabriel.bsb98@gmail.com>
2022-09-28 19:00:10 -0300

    :sparkles: Servico para adicionar usuario

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,7 +4,10 @@ import (
 	"log"
 	"net"
 
+	"github.com/codeedu/fc2-grpc/pb"
+	"github.com/codeedu/fc2-grpc/services"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -15,6 +18,8 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, &services.UserService{})
+	reflection.Register(grpcServer)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Could not serve: %v", err)
