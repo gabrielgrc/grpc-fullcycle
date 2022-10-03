8b0a7c2bc59fca48cb9c3b32d51fae6704cb8bc7
gabrielgrc <gabriel.bsb98@gmail.com>
2022-10-03 18:04:34 -0300

    :sparkles: Funcionalidade para realizar o stream na adicao de um usuario

--- services/user.go
+++ services/user.go
@@ -9,6 +9,7 @@ import (
 // type UserServiceServer interface {
 // 	AddUser(context.Context, *User) (*User, error)
 // 	mustEmbedUnimplementedUserServiceServer()
+//	AddUserVerbose(ctx context.Context, in *User, opts ...grpc.CallOption) (UserService_AddUserVerboseClient, error)
 // } 
 
 type UserService struct {
@@ -29,4 +30,46 @@ func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 		Name:  req.GetName(),
 		Email: req.GetEmail(),
 	}, nil
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Init",
+		User: &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserting",
+		User: &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "User has been inserted",
+		User: &pb.User{
+			Id: "123",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Completed",
+		User: &pb.User{
+			Id: "123",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	return nil
+
 }
