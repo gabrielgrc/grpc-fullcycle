cdf458cca126434aa4b69728f5ef5e0cea01b74e
gabrielgrc <gabriel.bsb98@gmail.com>
2022-10-10 22:29:42 -0300

    :sparkles: Implementacao do streaming bidirecional no client e no server

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -21,8 +21,8 @@ func main() {
 	client := pb.NewUserServiceClient(connection)
     //AddUser(client)
 	//AddUserVerbose(client)
-	AddUsers(client)
-	//AddUserStreamBoth(client)
+	//AddUsers(client)
+	AddUserStreamBoth(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -109,4 +109,68 @@ func AddUsers(client pb.UserServiceClient) {
 	}
 
 	fmt.Println(res)
+}
+
+func AddUserStreamBoth(client pb.UserServiceClient) {
+
+	stream, err := client.AddUserStreamBoth(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "w1",
+			Name:  "Wesley",
+			Email: "wes@wes.com",
+		},
+		&pb.User{
+			Id:    "w2",
+			Name:  "Wesley 2",
+			Email: "wes2@wes.com",
+		},
+		&pb.User{
+			Id:    "w3",
+			Name:  "Wesley 3",
+			Email: "wes3@wes.com",
+		},
+		&pb.User{
+			Id:    "w4",
+			Name:  "Wesley 4",
+			Email: "wes4@wes.com",
+		},
+		&pb.User{
+			Id:    "w5",
+			Name:  "Wesley 5",
+			Email: "wes5@wes.com",
+		},
+	}
+
+	wait := make(chan int)
+
+	go func() {
+		for _, req := range reqs {
+			fmt.Println("Sending user: ", req.Name)
+			stream.Send(req)
+			time.Sleep(time.Second * 2)
+		}
+		stream.CloseSend()
+	}()
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error receiving data: %v", err)
+				break
+			}
+			fmt.Printf("Recebendo user %v com status: %v\n", res.GetUser().GetName(), res.GetStatus())
+		}
+		close(wait)
+	}()
+
+	<-wait
 }
